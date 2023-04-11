a44ac1bbeb59967b42b7a09c582e957aeb2ca95f
Raphael Lima <phaelslima@gmail.com>
2023-04-10 21:35:30 -0300

    feat: configure status bar

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { ActivityIndicator, Text, View } from 'react-native'
+import { ActivityIndicator, StatusBar, Text, View } from 'react-native'
 import {
   useFonts,
   Roboto_400Regular,
@@ -9,7 +9,20 @@ export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#202024',
+      }}
+    >
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
       {fontsLoaded ? <Text>Hello World!</Text> : <ActivityIndicator />}
     </View>
   )
