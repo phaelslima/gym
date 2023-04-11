594e3313816141937338ddee9c342355d5c16729
Raphael Lima <phaelslima@gmail.com>
2023-04-11 20:27:26 -0300

    feat: add signUp screen

---
+++ src/screens/SignUp.tsx
@@ -0,0 +1,55 @@
+import { Center, Heading, Image, ScrollView, Text, VStack } from 'native-base'
+
+import BackgroundImg from '@assets/background.png'
+import LogoSvg from '@assets/logo.svg'
+
+import { Input } from '@components/Input'
+import { Button } from '@components/Button'
+
+export function SignUp() {
+  return (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} bg="gray.700" px={10}>
+        <Image
+          source={BackgroundImg}
+          alt="Pessoas treinando"
+          resizeMode="contain"
+          position="absolute"
+        />
+
+        <Center my={24}>
+          <LogoSvg />
+
+          <Text color="gray.100" fontSize="sm">
+            Treine sua mente e o seu corpo
+          </Text>
+        </Center>
+
+        <Center>
+          <Heading color="gray.100" fontSize="xl" fontFamily="heading" mb={6}>
+            Crie sua conta
+          </Heading>
+
+          <Input placeholder="Nome" />
+
+          <Input
+            placeholder="E-mail"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+
+          <Input placeholder="Senha" secureTextEntry />
+
+          <Input placeholder="Confirme  a Senha" secureTextEntry />
+
+          <Button title="Criar e acessar" />
+        </Center>
+
+        <Button title="Voltar para o login" variant="outline" mt={24} />
+      </VStack>
+    </ScrollView>
+  )
+}
