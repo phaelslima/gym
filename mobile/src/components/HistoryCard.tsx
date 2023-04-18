558b656f5963e149d0e88d10109498b7bc3b0326
Raphael Lima <phaelslima@gmail.com>
2023-04-18 20:56:15 -0300

    feat: add fetch history

--- mobile/src/components/HistoryCard.tsx
+++ mobile/src/components/HistoryCard.tsx
@@ -1,6 +1,12 @@
 import { Heading, HStack, Text, VStack } from 'native-base'
 
-export function HistoryCard() {
+import { HistoryDTO } from '@dtos/HistoryDTO'
+
+type Props = {
+  data: HistoryDTO
+}
+
+export function HistoryCard({ data }: Props) {
   return (
     <HStack
       w="full"
@@ -20,16 +26,16 @@ export function HistoryCard() {
           textTransform="capitalize"
           numberOfLines={1}
         >
-          Costas
+          {data.group}
         </Heading>
 
         <Text color="gray.100" fontSize="lg" numberOfLines={1}>
-          Puxada Frontal
+          {data.name}
         </Text>
       </VStack>
 
       <Text color="gray.300" fontSize="md">
-        08:56
+        {data.hour}
       </Text>
     </HStack>
   )
